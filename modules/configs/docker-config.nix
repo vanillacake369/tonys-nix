94114ef3080efa00fec8508bb72bf7ea2bea08fe
lim jihoon <limjihoon@MyLaptop>
2025-03-15 21:14:46 +0900

    Revert "fix : remove declaring user for docker"
    This reverts commit b8a0d59c8fd2b4ed317b3db1c7c81bb3da672a6a.

--- modules/configs/docker-config.nix
+++ modules/configs/docker-config.nix
@@ -1,10 +1,13 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, username, ... }: {
 
   # Enable Docker
   virtualisation.docker.enable = true;
 
   # Enable rootless Docker (optional)
   virtualisation.docker.rootless = false;
+
+  # Add user to Docker group
+  users.users.${username}.extraGroups = [ "docker" ];
 
 }
 
