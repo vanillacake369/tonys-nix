61cde28199a743f1b85c9abbcd26e967f96f3b47
lim jihoon <limjihoon@MyLaptop.>
2025-03-21 18:22:41 +0900

    feat : (on progress) remove that might overwrite container.conf

--- modules/pkgs/podman-pkgs.nix
+++ modules/pkgs/podman-pkgs.nix
@@ -1,13 +1,13 @@
 { pkgs, lib, ... }: {
 
   # Configure Podman setting
-  home.activation.configPodman = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    echo "$XDG_RUNTIME_DIR/podman"
-    if [ -d "$XDG_RUNTIME_DIR/podman" ]; then
-      nohup podman system service --time=0 > ~/.podman-service.log 2>&1 &
-      echo $! > ~/.podman-service.pid
-    fi
-  '';
+#  home.activation.configPodman = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+#    echo "$XDG_RUNTIME_DIR/podman"
+#    if [ -d "$XDG_RUNTIME_DIR/podman" ]; then
+#      nohup podman system service --time=0 > ~/.podman-service.log 2>&1 &
+#      echo $! > ~/.podman-service.pid
+#    fi
+#  '';
 
   home.file.".config/containers/containers.conf".text = ''
     [engine]
@@ -30,13 +30,13 @@
     # dockerCompat = true;
   };
 
-  home.activation.exposePodman = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    systemctl --user start podman.socket || true
-    export PODMAN_SYSTEMD_UNIT=podman.socket
-  '';
+#  home.activation.exposePodman = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+#    systemctl --user start podman.socket || true
+#    export PODMAN_SYSTEMD_UNIT=podman.socket
+#  '';
 
   home.sessionVariables = {
     DOCKER_HOST = "unix:///run/user/1000/podman/podman.sock";
   };
 }
-
+#
