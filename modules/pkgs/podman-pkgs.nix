9f776570e9ab5f9bd2da67e1a54d9249ff82a851
lim jihoon <limjihoon@MyLaptop.>
2025-03-21 18:23:30 +0900

    feat : (on progress) remove that delcare container.conf

--- modules/pkgs/podman-pkgs.nix
+++ modules/pkgs/podman-pkgs.nix
@@ -1,18 +1,18 @@
 { pkgs, lib, ... }: {
 
   # Configure Podman setting
-#  home.activation.configPodman = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-#    echo "$XDG_RUNTIME_DIR/podman"
-#    if [ -d "$XDG_RUNTIME_DIR/podman" ]; then
-#      nohup podman system service --time=0 > ~/.podman-service.log 2>&1 &
-#      echo $! > ~/.podman-service.pid
-#    fi
-#  '';
-
-  home.file.".config/containers/containers.conf".text = ''
-    [engine]
-    cgroup_manager = "cgroupfs"
+  home.activation.configPodman = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    echo "$XDG_RUNTIME_DIR/podman"
+    if [ -d "$XDG_RUNTIME_DIR/podman" ]; then
+      nohup podman system service --time=0 > ~/.podman-service.log 2>&1 &
+      echo $! > ~/.podman-service.pid
+    fi
   '';
+
+# home.file.".config/containers/containers.conf".text = ''
+#   [engine]
+#   cgroup_manager = "cgroupfs"
+# '';
 
   home.packages = with pkgs; [
     qemu # required for `podman machine init`
@@ -30,10 +30,10 @@
     # dockerCompat = true;
   };
 
-#  home.activation.exposePodman = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-#    systemctl --user start podman.socket || true
-#    export PODMAN_SYSTEMD_UNIT=podman.socket
-#  '';
+  home.activation.exposePodman = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    systemctl --user start podman.socket || true
+    export PODMAN_SYSTEMD_UNIT=podman.socket
+  '';
 
   home.sessionVariables = {
     DOCKER_HOST = "unix:///run/user/1000/podman/podman.sock";
