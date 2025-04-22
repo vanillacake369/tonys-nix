45ad14808142a78f3e6d8bf3ca2475b36cb0f250
limjihoon <lonelynight1026@gmail.com>
2025-04-23 01:38:00 +0900

    feat : flake.nix > add overlays in pkgs of input

--- flake.nix
+++ flake.nix
@@ -19,7 +19,10 @@
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs { 
+        inherit system;
+        overlays = [ self.overlays.default ];
+      };
     in {
       # Overlay on chromium for wayland multi gesture
       overlays.default = (final: prev: {
