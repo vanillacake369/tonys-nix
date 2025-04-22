7d10547a1d4659c7416af15e3d1a45fa96b8e50c
limjihoon <lonelynight1026@gmail.com>
2025-04-23 00:08:54 +0900

    feat : add goland,chrome,youtube-music keybinding of dconf

--- limjihoon-user.nix
+++ limjihoon-user.nix
@@ -2,25 +2,4 @@
   home.username = "limjihoon";
   home.homeDirectory = "/home/limjihoon";
   home.stateVersion = "23.11"; # Don't change after first setup
-  dconf.settings = {
-    "org/gnome/settings-daemon/plugins/media-keys" = {
-        next = [ "<Shift><Control>n" ];
-        previous = [ "<Shift><Control>p" ];
-        play = [ "<Shift><Control>space" ];
-        custom-keybindings = [
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
-        ];
-      };
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-        name = "gnome console";
-        command = "kgx";
-        binding = "<Ctrl><Alt>t";
-      };
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
-        name = "intellij";
-        command = "idea-ultimate";
-        binding = "<Ctrl><Alt>j";
-      };
-  };
 }
--- modules/config/hotkey-pkgs.nix
+++ modules/config/hotkey-pkgs.nix
@@ -1,12 +1,39 @@
 dconf.settings = {
   "org/gnome/settings-daemon/plugins/media-keys" = {
-    custom-keybindings = [
-      "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-    ];
-  };
-  "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-    binding = "<Primary><Alt>t";
-    command = "gnome-terminal";
-    name = "open-terminal";
-  };
+      next = [ "<Shift><Control>n" ];
+      previous = [ "<Shift><Control>p" ];
+      play = [ "<Shift><Control>space" ];
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/"
+      ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      name = "gnome console";
+      command = "kgx";
+      binding = "<Ctrl><Alt>t";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+      name = "intellij";
+      command = "idea-ultimate";
+      binding = "<Ctrl><Alt>i";
+    } 
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+      name = "goland";
+      command = "goland";
+      binding = "<Ctrl><Alt>g";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
+      name = "google chrome";
+      command = "google-chrome-stable";
+      binding = "<Ctrl><Alt>c";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4" = {
+      name = "youtube music";
+      command = "youtube-music";
+      binding = "<Ctrl><Alt>m";
+    };
 };
