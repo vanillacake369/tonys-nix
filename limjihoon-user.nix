07d6c637874ba9190c07cb194e2332a54b16637f
limjihoon <lonelynight1026@gmail.com>
2025-04-22 23:33:20 +0900

    feat : add keybinding of dconf

--- limjihoon-user.nix
+++ limjihoon-user.nix
@@ -2,4 +2,19 @@
   home.username = "limjihoon";
   home.homeDirectory = "/home/limjihoon";
   home.stateVersion = "23.11"; # Don't change after first setup
+  dconf.settings = {
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+        next = [ "<Shift><Control>n" ];
+        previous = [ "<Shift><Control>p" ];
+        play = [ "<Shift><Control>space" ];
+        custom-keybindings = [
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+        ];
+      };
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+        name = "kgx";
+        command = "kgx";
+        binding = "<Ctrl><Alt>t";
+      };
+  };
 }
---
+++ modules/config/hotkey-pkgs.nix
@@ -0,0 +1,12 @@
+dconf.settings = {
+  "org/gnome/settings-daemon/plugins/media-keys" = {
+    custom-keybindings = [
+      "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+    ];
+  };
+  "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+    binding = "<Primary><Alt>t";
+    command = "gnome-terminal";
+    name = "open-terminal";
+  };
+};
