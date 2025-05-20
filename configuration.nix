5aafa8fc5cbf911bdc308d68809619dbc8f0be9e
limjihoon <lonelynight1026@gmail.com>
2025-05-20 22:38:42 +0900

    feat : disable kvm

--- configuration.nix
+++ configuration.nix
@@ -16,6 +16,8 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
+    kernelParams = [ "kvm.enable_virt_at_load=0" ];
+    blacklistedKernelModules = [ "kvm" "kvm_intel" "kvm_amd" ];
   };
 
   networking = {
