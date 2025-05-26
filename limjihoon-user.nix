3f1b759c9e25e4d209ce810fb9bdafbeeacd2e36
limjihoon <lonelynight1026@gmail.com>
2025-05-27 08:12:01 +0900

    fix : keybind of volume control

--- limjihoon-user.nix
+++ limjihoon-user.nix
@@ -6,8 +6,8 @@
   # Applied keybindings :: https://heywoodlh.io/nixos-gnome-settings-and-keyboard-shortcuts
   dconf.settings = {
     "org/gnome/settings-daemon/plugins/media-keys" = {
-        volume-up = [ "<Shift><Control>Up" ];
-        volume-down = [ "<Shift><Control>Down" ];
+        volume-up = [ "<Ctrl><Alt>Up" ];
+        volume-down = [ "<Ctrl><Alt>Down" ];
         next = [ "<Shift><Control>n" ];
         previous = [ "<Shift><Control>p" ];
         play = [ "<Shift><Control>space" ];
