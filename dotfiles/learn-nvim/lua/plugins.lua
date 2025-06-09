093b4b55e4c0abc4801e85739740bf22af91ca30
limjihoon <lonelynight1026@gmail.com>
2025-06-10 08:45:24 +0900

    feat : use packer plugin manager in lua

---
+++ dotfiles/learn-nvim/lua/plugins.lua
@@ -0,0 +1,13 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  print("Installing packer close and reopen Neovim...")
+end
+
+-- Install plugins here
+return require('packer').startup(function(use)
+ use 'wbthomason/packer.nvim'
+end)
