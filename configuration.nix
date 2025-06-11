466340b65c02bd0f9ed6da87d730e28d2becc457
limjihoon <lonelynight1026@gmail.com>
2025-06-12 04:35:16 +0900

    feat : enable google mfa on ssh

--- configuration.nix
+++ configuration.nix
@@ -99,9 +99,10 @@ in
         PasswordAuthentication = false;
         AllowUsers = null;
         UseDns = false;
-        X11Forwarding = false;
-        PermitRootLogin = "prohibit-password";
-        KbdInteractiveAuthentication = false;
+        X11Forwarding = true;
+        PermitRootLogin = "no";
+        KbdInteractiveAuthentication = true;
+        AuthenticationMethods = "publickey,keyboard-interactive";
       };
     };
     fail2ban.enable = true;
@@ -112,6 +113,23 @@ in
         auth-user-pass ${userHome}/my-nixos/openvpn/secret
       '';
       updateResolvConf = true;
+    };
+  };
+  # Google MFA on SSH
+  security.pam.services = {
+    sshd = {
+      text = ''
+        account required pam_unix.so # unix (order 10900)
+
+        auth required ${pkgs.google-authenticator}/lib/security/pam_google_authenticator.so nullok no_increment_hotp # google_authenticator (order 12500)
+        auth sufficient pam_permit.so
+
+        session required pam_env.so conffile=/etc/pam/environment readenv=0 # env (order 10100)
+        session required pam_unix.so # unix (order 10200)
+        session required pam_loginuid.so # loginuid (order 10300)
+        session optional ${pkgs.systemd}/lib/security/pam_systemd.so # systemd (order 12000)
+      '';
+      googleAuthenticator.enable = true;
     };
   };
 
