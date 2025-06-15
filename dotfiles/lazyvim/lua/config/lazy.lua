0edbbd840973353ba70eb9170909289d6360ee75
limjihoon <lonelynight1026@gmail.com>
2025-06-15 17:20:41 +0900

    feat : add java,go on lazy.nvim

--- dotfiles/lazyvim/lua/config/lazy.lua
+++ dotfiles/lazyvim/lua/config/lazy.lua
@@ -22,6 +22,8 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "plugins" },
   },
+  -- Write changes to symlinked lazy-lock.json
+  lockfile = vim.fn.stdpath("state") .. "/lazy-lock.json",
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
--- dotfiles/lazyvim/lua/plugins/plugins.lua
+++ dotfiles/lazyvim/lua/plugins/plugins.lua
@@ -59,111 +59,75 @@ return {
     },
   },
 
-  -- lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    name = "lspconfig.nixd",
-    ft = { "nix" },
-    opts = {},
-    config = function(_, opts)
-      require("lspconfig").nixd.setup(opts)
-    end,
-  },
---   {
---     "neovim/nvim-lspconfig",
---     ---@class PluginLspOpts
---     opts = {
---       ---@type lspconfig.options
---       servers = {
---         -- Nix
---         nixd = {
---           cmd = { "nixd" },
---           settings = {
---             nixd = {
---               nixpkgs = {
---                 -- For flake.
---                 -- This expression will be interpreted as "nixpkgs" toplevel
---                 -- Nixd provides package, lib completion/information from it.
---                 -- Resource Usage: Entries are lazily evaluated, entire nixpkgs takes 200~300MB for just "names".
---                 -- Package documentation, versions, are evaluated by-need.
---                 expr = "import (builtins.getFlake(toString ./.)).inputs.nixpkgs { }",
---               },
---               formatting = {
---                 command = { "nixfmt" }, -- possiuble options : nixfmt ,nixpkgs-fmt, alejandra
---               },
---               options = {
---                 nixos = {
---                   expr = "let flake = builtins.getFlake(toString ./.); in flake.nixosConfigurations.limjihoon.options",
---                 },
---                 home_manager = {
---                   expr = 'let flake = builtins.getFlake(toString ./.); in flake.homeConfigurations.limjihoon.options',
---                 },
---               },
---             },
---           },
---         },
---       },
---     },
---   },
-
-  -- conform.nvim
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        nix = { "nixfmt" },
-      },
-      formatters = {
-        nixfmt = {
-          command = "nixfmt",
-          args = {},
-          stdin = true,
-        },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
-    },
-  },
-
   -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
         "bash",
-        "html",
-        "javascript",
         "json",
         "lua",
         "markdown",
         "markdown_inline",
-        "python",
         "query",
         "regex",
-        "tsx",
-        "typescript",
         "vim",
         "yaml",
         "nix",
+        "go",
+        "gomod", 
+        "gowork", 
+        "gosum",
+        "java",
+        "dockerfile",
       },
     },
   },
 
-  -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
-  -- would overwrite `ensure_installed` with the new value.
-  -- If you'd rather extend the default config, use the code below instead:
+  -- lspconfig
   {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- add tsx and treesitter
-      vim.list_extend(opts.ensure_installed, {
-        "tsx",
-        "typescript",
-      })
+    "neovim/nvim-lspconfig",
+    name = "lspconfig.nixd",
+    ft = { "nix" },
+    opts = {
+      servers = {
+        nixd = {
+          cmd = { "nixd" },
+          settings = {
+            nixd = {
+              nixpkgs = {
+                expr = "import (builtins.getFlake(toString ./.)).inputs.nixpkgs { }",
+              },
+              formatting = {
+                command = { "alejandra" },
+              },
+              options = {
+                nixos = {
+                  expr = "let flake = builtins.getFlake(toString ./.); in flake.nixosConfigurations.limjihoon.options",
+                },
+                home_manager = {
+                  expr = 'let flake = builtins.getFlake(toString ./.); in flake.homeConfigurations.limjihoon.options',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("lspconfig").nixd.setup(opts)
     end,
   },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   optional = true,
+  --   opts = {
+  --     formatters_by_ft = {
+  --       nix = { "nixfmt" },
+  --     },
+  --   },
+  -- }
+
 
   -- the opts function can also be used to change the default opts:
   {
@@ -189,21 +153,17 @@ return {
     end,
   },
 
-  -- use mini.starter instead of alpha
-  { import = "lazyvim.plugins.extras.ui.mini-starter" },
-
-  -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
-  { import = "lazyvim.plugins.extras.lang.json" },
-
   -- add any tools you want to have installed below
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
         "stylua",
         "shellcheck",
         "shfmt",
         "flake8",
+        "gomodifytags", 
+        "impl",
       },
     },
   },
