dda753e6caa744e1a88bf062d9813485a6f07e68
limjihoon <lonelynight1026@gmail.com>
2025-06-16 23:50:51 +0900

    feat : add formatter of golang, lua

--- modules/language.nix
+++ modules/language.nix
@@ -1,19 +1,21 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
 
   home.packages = with pkgs; [
     zulu17
     gradle
     just
     go
+    gotools
+    gofumpt
     gcc
     lua54Packages.lua
     lua54Packages.luaunit
+    stylua
     nixd
     nixfmt-rfc-style
-    # nixfmt-classic
   ];
   home.sessionVariables = {
     JAVA_HOME = "${pkgs.zulu17}";
   };
 }
-
