4128a57d538468f47bd2a003dc364c3fc6ab216a
limjihoon <lonelynight1026@hamagroups.io>
2025-07-28 11:57:31 +0900

    feat : builders.nix > general builder attribute set to remove duplication in flake.nix

--- flake.nix
+++ flake.nix
@@ -28,44 +28,40 @@
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       
-      mkSystem = system:
-        let
-          # Overlays on pkgs
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              (final: prev: {
-                google-chrome = prev.google-chrome.override {
-                  commandLineArgs = "--ozone-platform-hint=auto --enable-wayland-ime --enable-features=TouchpadOverscrollHistoryNavigation --wayland-text-input-version=3";
-                };
-                slack = final.symlinkJoin {
-                  name = "slack";
-                  paths = [ prev.slack ];
-                  buildInputs = [ final.makeWrapper ];
-                  postBuild = ''
-                    wrapProgram $out/bin/slack \
-                      --add-flags "--ozone-platform-hint=auto --enable-wayland-ime --enable-features=TouchpadOverscrollHistoryNavigation --wayland-text-input-version=3"
-                  '';
-                };
-              })
-            ];
-            config.allowUnfree = true;
+      # Define overlays for package customizations
+      overlays = [
+        (final: prev: {
+          google-chrome = prev.google-chrome.override {
+            commandLineArgs = "--ozone-platform-hint=auto --enable-wayland-ime --enable-features=TouchpadOverscrollHistoryNavigation --wayland-text-input-version=3";
           };
-          # Pass boolean flag of linux or darwin
-          isLinux = pkgs.stdenv.isLinux;
-          isDarwin = pkgs.stdenv.isDarwin;
-        in {
-          inherit pkgs isLinux isDarwin;
-        };
+          slack = final.symlinkJoin {
+            name = "slack";
+            paths = [ prev.slack ];
+            buildInputs = [ final.makeWrapper ];
+            postBuild = ''
+              wrapProgram $out/bin/slack \
+                --add-flags "--ozone-platform-hint=auto --enable-wayland-ime --enable-features=TouchpadOverscrollHistoryNavigation --wayland-text-input-version=3"
+            '';
+          };
+        })
+      ];
+      
+      # Shared home-manager modules
+      homeManagerModules = [
+        ./home.nix
+        ./limjihoon-user.nix
+      ];
+      
+      # Import builders from lib directory
+      builders = import ./lib/builders.nix { inherit nixpkgs home-manager homeManagerModules overlays; };
       
       # Support multiple hostname configurations
-      # Add hostnames here to support justfile's {{HOSTNAME}} variable
       hostnames = [ "HAMA" "nixos" ];
     in
     {
       # Define nixos configuration for multiple hostnames
       nixosConfigurations = lib.genAttrs hostnames (hostname: 
-        let systemConfig = mkSystem "x86_64-linux";
+        let systemConfig = builders.mkSystem "x86_64-linux";
         in nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           pkgs = systemConfig.pkgs;
@@ -74,61 +70,15 @@
           ];
         });
       
-      # Define the home-manager configuration for each supported system
-      homeConfigurations = forAllSystems (system:
-        let systemConfig = mkSystem system;
-        in {
-          # WSL Home Manager configuration
-          hm-wsl = home-manager.lib.homeManagerConfiguration {
-            pkgs = systemConfig.pkgs;
-            modules = [
-              ./home.nix
-              ./limjihoon-user.nix
-            ];
-            extraSpecialArgs = {
-              inherit (systemConfig) isLinux isDarwin;
-              isWsl = true;
-            };
-          };
-          # NixOS Home Manager configuration
-          hm-nixos = home-manager.lib.homeManagerConfiguration {
-            pkgs = systemConfig.pkgs;
-            modules = [
-              ./home.nix
-              ./limjihoon-user.nix
-            ];
-            extraSpecialArgs = {
-              inherit (systemConfig) isLinux isDarwin;
-              isWsl = false;
-            };
-          };
-        }) // {
-          # Default configurations for current system (fallback)
-          hm-wsl = let systemConfig = mkSystem "x86_64-linux"; in
-            home-manager.lib.homeManagerConfiguration {
-              pkgs = systemConfig.pkgs;
-              modules = [
-                ./home.nix
-                ./limjihoon-user.nix
-              ];
-              extraSpecialArgs = {
-                inherit (systemConfig) isLinux isDarwin;
-                isWsl = true;
-              };
-            };
-          hm-nixos = let systemConfig = mkSystem "x86_64-linux"; in
-            home-manager.lib.homeManagerConfiguration {
-              pkgs = systemConfig.pkgs;
-              modules = [
-                ./home.nix
-                ./limjihoon-user.nix
-              ];
-              extraSpecialArgs = {
-                inherit (systemConfig) isLinux isDarwin;
-                isWsl = false;
-              };
-            };
-        };
+      # Clean home-manager configurations without duplication
+      # Primary -> WSL, NixOS
+      # Secondary(fallback) -> Other OS
+      homeConfigurations = {
+        hm-wsl = builders.mkHomeConfig { system = "x86_64-linux"; isWsl = true; };
+        hm-nixos = builders.mkHomeConfig { system = "x86_64-linux"; isWsl = false; };
+      } // (forAllSystems (system: {
+        "hm-${system}" = builders.mkHomeConfig { inherit system; isWsl = false; };
+      }));
       
       # Define system manager to cope with linux distro system
       systemConfigs.default = system-manager.lib.makeSystemConfig {
---
+++ lib/builders.nix
@@ -0,0 +1,32 @@
+{ nixpkgs, home-manager, homeManagerModules, overlays }:
+let
+
+
+  mkSystem = system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        inherit overlays;
+        config.allowUnfree = true;
+      };
+      isLinux = pkgs.stdenv.isLinux;
+      isDarwin = pkgs.stdenv.isDarwin;
+    in {
+      inherit pkgs isLinux isDarwin;
+    };
+
+  # Reusable home-manager configuration builder
+  mkHomeConfig = { system, isWsl ? false }:
+    let systemConfig = mkSystem system;
+    in home-manager.lib.homeManagerConfiguration {
+      pkgs = systemConfig.pkgs;
+      modules = homeManagerModules;
+      extraSpecialArgs = {
+        inherit (systemConfig) isLinux isDarwin;
+        inherit isWsl;
+      };
+    };
+in
+{
+  inherit mkSystem mkHomeConfig;
+}
