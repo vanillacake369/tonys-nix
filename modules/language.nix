3768fa7be1bcf7f8e7a0dad28f902e743a3b4258
limjihoon <lonelynight1026@gmail.com>
2025-08-04 11:48:48 +0900

    feat : add uv (python package installer)

--- modules/language.nix
+++ modules/language.nix
@@ -1,21 +1,27 @@
-{ pkgs, ... }:
-{
-
+{pkgs, ...}: {
   home.packages = with pkgs; [
+    # Java
     zulu17
     gradle
+    # Just
     just
+    # Make
     gnumake
+    # Go
     go
     gotools
     gofumpt
+    # Lua
     gcc
     lua54Packages.lua
     lua54Packages.luaunit
     stylua
+    # Nix
     nixd
     nixfmt-rfc-style
     alejandra
+    # Python
+    uv
   ];
   home.sessionVariables = {
     JAVA_HOME = "${pkgs.zulu17}";
