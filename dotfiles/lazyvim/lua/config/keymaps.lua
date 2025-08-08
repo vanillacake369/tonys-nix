301b45034f318460ec250a76bc880d3cc310f146
limjihoon <lonelynight1026@gmail.com>
2025-08-08 16:41:22 +0900

    feat : stroll suggestion using arrow in nvim

--- dotfiles/lazyvim/lua/config/keymaps.lua
+++ dotfiles/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Completion navigation with arrows
+vim.keymap.set("i", "<C-Down>", function()
+  if vim.fn.pumvisible() == 1 then
+    return "<C-n>"
+  else
+    return "<Down>"
+  end
+end, { expr = true, desc = "Navigate down in completion menu" })
+
+vim.keymap.set("i", "<C-Up>", function()
+  if vim.fn.pumvisible() == 1 then
+    return "<C-p>"
+  else
+    return "<Up>"
+  end
+end, { expr = true, desc = "Navigate up in completion menu" })
