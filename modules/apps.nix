aaca6dfc8ce580bc15efa71d919a19608898d833
limjihoon <lonelynight1026@gmail.com>
2025-08-12 20:21:20 +0900

    feat : add raycast

--- modules/apps.nix
+++ modules/apps.nix
@@ -43,21 +43,6 @@
       yabai
       skhd
       keycastr
+      raycast
     ];
-
-  # Create app symlinks for macOS Spotlight integration
-  home.file = lib.mkIf isDarwin {
-    "Applications/Google Chrome.app".source = "${pkgs.google-chrome}/Applications/Google Chrome.app";
-    "Applications/IntelliJ IDEA.app".source = "${pkgs.jetbrains.idea-ultimate}/Applications/IntelliJ IDEA.app";
-    "Applications/GoLand.app".source = "${pkgs.jetbrains.goland}/Applications/GoLand.app";
-    "Applications/DataGrip.app".source = "${pkgs.jetbrains.datagrip}/Applications/DataGrip.app";
-    "Applications/draw.io.app".source = "${pkgs.drawio}/Applications/draw.io.app";
-    "Applications/Discord.app".source = "${pkgs.discord}/Applications/Discord.app";
-    "Applications/Obsidian.app".source = "${pkgs.obsidian}/Applications/Obsidian.app";
-    "Applications/WezTerm.app".source = "${pkgs.wezterm}/Applications/WezTerm.app";
-    "Applications/Hidden Bar.app".source = "${pkgs.hidden-bar}/Applications/Hidden Bar.app";
-    "Applications/AlDente.app".source = "${pkgs.aldente}/Applications/AlDente.app";
-    "Applications/Bartender 4.app".source = "${pkgs.bartender}/Applications/Bartender 4.app";
-    "Applications/KeyCastr.app".source = "${pkgs.keycastr}/Applications/KeyCastr.app";
-  };
 }
