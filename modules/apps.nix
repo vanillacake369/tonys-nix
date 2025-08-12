96ad0e1dc17bbee9b426cf7041e1f65d2c44bdff
limjihoon <lonelynight1026@gmail.com>
2025-08-12 19:08:03 +0900

    feat : configure symlink for spotlight to find nix installed packages

--- modules/apps.nix
+++ modules/apps.nix
@@ -5,7 +5,29 @@
   isDarwin,
   isLinux,
   ...
-}: {
+}: let
+  # GUI packages that need Spotlight integration on macOS
+  guiPackages = lib.optionals (!isWsl) [
+    { pkg = pkgs.google-chrome; name = "Google Chrome"; }
+    { pkg = pkgs.jetbrains.idea-ultimate; name = "IntelliJ IDEA"; }
+    { pkg = pkgs.jetbrains.goland; name = "GoLand"; }
+    { pkg = pkgs.jetbrains.datagrip; name = "DataGrip"; }
+    { pkg = pkgs.drawio; name = "draw.io"; }
+    { pkg = pkgs.discord; name = "Discord"; }
+    { pkg = pkgs.obsidian; name = "Obsidian"; }
+    { pkg = pkgs.wezterm; name = "WezTerm"; }
+  ] ++ lib.optionals isDarwin [
+    { pkg = pkgs.hidden-bar; name = "Hidden Bar"; }
+    { pkg = pkgs.aldente; name = "AlDente"; }
+    { pkg = pkgs.bartender; name = "Bartender 4"; }
+    { pkg = pkgs.keycastr; name = "KeyCastr"; }
+  ];
+
+  # Simple function to create app symlinks for Spotlight
+  mkAppLink = app: {
+    "Applications/${app.name}.app".source = "${app.pkg}/Applications/${app.name}.app";
+  };
+in {
   home.packages = with pkgs;
     [
       claude-code
@@ -46,4 +68,7 @@
       # Slack has known issues on macOS Sequoia, may need Homebrew fallback
       # slack
     ];
+
+  # Create app symlinks for macOS Spotlight integration
+  home.file = lib.mkIf isDarwin (lib.mkMerge (map mkAppLink guiPackages));
 }
