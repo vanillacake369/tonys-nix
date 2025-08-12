562d9386ef2f263f441d4ad731a81d259a4b1161
limjihoon <lonelynight1026@gmail.com>
2025-08-13 03:01:27 +0900

    fix : configure nixd, alejandra

--- dotfiles/lazyvim/lua/plugins/langs/nix.lua
+++ dotfiles/lazyvim/lua/plugins/langs/nix.lua
@@ -1,20 +1,43 @@
 return {
-  -- Install alejandra automatically
+  -- Mason LSP configuration for automatic server management
   {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = { "williamboman/mason.nvim" },
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "nixd", -- Ensure nixd LSP server is installed via Mason
+      })
+    end,
+  },
+
+  -- LSP configuration
+  {
+    "neovim/nvim-lspconfig",
     opts = {
-      ensure_installed = { "alejandra" },
+      servers = {
+        nixd = {
+          settings = {
+            nixd = {
+              nixpkgs = {
+                expr = "import <nixpkgs> { }",
+              },
+              formatting = {
+                command = { "alejandra" },
+              },
+            },
+          },
+        },
+      },
     },
   },
 
-  -- Configure Conform to use alejandra
+  -- Configure Conform to use alejandra for .nix files
   {
     "stevearc/conform.nvim",
     opts = function(_, opts)
-      opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft or {}, {
-        nix = { "alejandra" },
-      })
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.nix = { "alejandra" }
+      return opts
     end,
   },
 }
