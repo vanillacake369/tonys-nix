b6f79801ad75e2ad1163ded3afef267882dda070
limjihoon <lonelynight1026@hamagroups.io>
2025-08-18 10:11:54 +0900

    feat : jetbrains.nix > bind keymap.xml by host system

--- lib/builders.nix
+++ lib/builders.nix
@@ -1,24 +1,29 @@
-{ nixpkgs, home-manager, homeManagerModules, overlays }:
-let
-
-
-  mkSystem = system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        inherit overlays;
-        config.allowUnfree = true;
-      };
-      isLinux = pkgs.stdenv.isLinux;
-      isDarwin = pkgs.stdenv.isDarwin;
-    in {
-      inherit pkgs isLinux isDarwin;
+{
+  nixpkgs,
+  home-manager,
+  homeManagerModules,
+  overlays,
+}: let
+  mkSystem = system: let
+    pkgs = import nixpkgs {
+      inherit system;
+      inherit overlays;
+      config.allowUnfree = true;
     };
+    isLinux = pkgs.stdenv.isLinux;
+    isDarwin = pkgs.stdenv.isDarwin;
+  in {
+    inherit pkgs isLinux isDarwin;
+  };
 
   # Reusable home-manager configuration builder
-  mkHomeConfig = { system, isWsl ? false }:
-    let systemConfig = mkSystem system;
-    in home-manager.lib.homeManagerConfiguration {
+  mkHomeConfig = {
+    system,
+    isWsl ? false,
+  }: let
+    systemConfig = mkSystem system;
+  in
+    home-manager.lib.homeManagerConfiguration {
       pkgs = systemConfig.pkgs;
       modules = homeManagerModules;
       extraSpecialArgs = {
@@ -26,7 +31,6 @@ let
         inherit isWsl;
       };
     };
-in
-{
+in {
   inherit mkSystem mkHomeConfig;
 }
