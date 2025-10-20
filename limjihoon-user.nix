3edf309c19ec78998b2854c29b4d926c5316f1cc
limjihoon <lonelynight1026@gmail.com>
2025-10-20 11:51:27 +0900

    format : limjihoon-user.nix
    remove : script of aquanuri-db-connection & vpn-connection

--- limjihoon-user.nix
+++ limjihoon-user.nix
@@ -1,14 +1,8 @@
-{
-  lib,
-  config,
-  pkgs,
-  isWsl ? false,
-  ...
-}: {
+{pkgs, ...}: {
   home.username = "limjihoon";
-  home.homeDirectory = if pkgs.stdenv.isDarwin then "/Users/limjihoon" else "/home/limjihoon";
+  home.homeDirectory =
+    if pkgs.stdenv.isDarwin
+    then "/Users/limjihoon"
+    else "/home/limjihoon";
   home.stateVersion = "23.11"; # Don't change after first setup
-
-  # GNOME dconf settings moved to nixos-modules/gnome-settings.nix
-  # Import that module in home.nix or configuration.nix for GNOME desktop systems
 }
