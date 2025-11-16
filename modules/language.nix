bfc1f25302b6bd631783d84fbdec11712a8e7b4e
limjihoon <lonelynight1026@gmail.com>
2025-11-16 22:41:45 +0900

    feat : language.nix > add lsp support
    Add language servers for improved IDE experience in neovim:
    - jdt-language-server (Java)
    - gopls (Go)
    - lua-language-server (Lua)
    - yaml-language-server (YAML)
    - typescript-language-server (TypeScript/JavaScript)

--- modules/language.nix
+++ modules/language.nix
@@ -3,27 +3,40 @@
     # Java
     zulu17
     gradle
+    jdt-language-server
+
     # Just
     just
+
     # Make
     gnumake
+
     # Go
     go
     gotools
+    gopls
+
     # Lua
     gcc
     lua54Packages.lua
     lua54Packages.luaunit
-    # Python
-    uv
-    # Node.js (for Mason LSP installations)
-    nodejs_20
+    lua-language-server
+
     # Rust (for cargo-based tools)
     cargo
     rustc
+
     # Nix development tools
-    nixd              # Nix language server
-    alejandra         # Nix formatter
+    nixd
+    alejandra
+
+    # YAML
+    yamllint
+    yaml-language-server
+
+    # TypeScript/JavaScript
+    nodejs_22
+    nodePackages.typescript-language-server
   ];
   home.sessionVariables = {
     JAVA_HOME = "${pkgs.zulu17}";
