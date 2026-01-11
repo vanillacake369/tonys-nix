875d0747b607c0ea0831a3ed8ed5801155151f81
limjihoon <lonelynight1026@gmail.com>
2026-01-12 04:03:21 +0900

    feat : switch brave from chrome

--- modules/apps.nix
+++ modules/apps.nix
@@ -1,47 +1,44 @@
 {
-  lib,
-  pkgs,
-  isWsl,
-  isDarwin,
-  isLinux,
-  ...
+lib,
+pkgs,
+isWsl,
+isDarwin,
+isLinux,
+...
 }: {
-  home.packages = with pkgs;
-    [
-      # General apps
-      claude-code
-    ]
-    ++ lib.optionals (!isWsl) [
-      # Non WSL apps
-      google-chrome
-      obsidian
-      jetbrains.idea
-      jetbrains.goland
-      jetbrains.datagrip
-    ]
-    ++ lib.optionals (isLinux && !isWsl) [
-      # Linux-specific apps
-      firefox
-      slack
-      ticktick
-      ytmdesktop
-      libreoffice
-      hunspell
-      hunspellDicts.en_US
-      hunspellDicts.ko_KR
-      hunspellDicts.ko-kr
-    ]
-    ++ lib.optionals isDarwin [
-      # MacOs Apps
-      aldente
-      yabai
-      skhd
-      raycast
-      jankyborders
-      appcleaner
-      hidden-bar
-      wezterm
-      aerospace
-      # shottr
-    ];
+    home.packages = with pkgs;
+        [
+            # General apps
+            claude-code
+        ]
+        ++ lib.optionals (!isWsl) [
+            # Non WSL apps
+            brave
+            jetbrains.idea
+            jetbrains.goland
+            jetbrains.datagrip
+        ]
+        ++ lib.optionals (isLinux && !isWsl) [
+            # Linux-specific apps
+            firefox
+            slack
+            ticktick
+            ytmdesktop
+            libreoffice
+            hunspell
+            hunspellDicts.en_US
+            hunspellDicts.ko_KR
+            hunspellDicts.ko-kr
+        ]
+        ++ lib.optionals isDarwin [
+            # MacOs Apps
+            aldente
+            raycast
+            jankyborders
+            appcleaner
+            hidden-bar
+            wezterm
+            aerospace
+            # shottr
+        ];
 }
