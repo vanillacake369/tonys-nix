d7e5474c77a411d3e9c1ef6233fa9656b83bc1ca
limjihoon <lonelynight1026@gmail.com>
2026-05-21 00:08:50 +0900

    chore : cleanup > remove dead limjihoon-user.nix

--- limjihoon-user.nix
+++
@@ -1,8 +0,0 @@
-{pkgs, ...}: {
-  home.username = "limjihoon";
-  home.homeDirectory =
-    if pkgs.stdenv.isDarwin
-    then "/Users/limjihoon"
-    else "/home/limjihoon";
-  home.stateVersion = "23.11"; # Don't change after first setup
-}
