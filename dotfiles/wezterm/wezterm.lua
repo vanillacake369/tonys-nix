485659a7bbdd56c346a67f1732a13803bfbc1778
limjihoon <lonelynight1026@gmail.com>
2026-07-01 13:08:56 +0900

    refactor : dotfile > optimize wezterm's tab

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 ----------------------------------------------------------------
 -- 폰트 및 렌더링
 ----------------------------------------------------------------
--- font_with_fallback을 하나로 합쳐 무시되는 설정을 방지합니다.
+-- font_with_fallback을 하나로 합쳐 무시되는 설정을 방지
 config.font = wezterm.font_with_fallback({
     "JetBrains Mono",
     "Roboto",
@@ -14,21 +14,36 @@ config.font_size = 10
 config.font_shaper = "Harfbuzz"
 
 ----------------------------------------------------------------
--- GPU 가속 및 디스플레이 최적화 (성능 체감 제일 큼)
+-- 디스플레이 최적화
+----------------------------------------------------------------
+-- 타이틀 바를 숨겨서 tty/pty 공간 확보
+config.window_decorations = "RESIZE"
+-- 탭 바를 극도로 단순화하여 tty/pty 공간 확보
+config.hide_tab_bar_if_only_one_tab = true
+-- 클래식 디자인 탭 바를 사용하여 tty/pty 공간 확보
+config.enable_tab_bar = false
+-- 창 내부 여백을 0으로 만들어 tty/pty 공간 확보
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+
+----------------------------------------------------------------
+-- GPU 최적화
 ----------------------------------------------------------------
 -- 없는 글자(글리프)를 찾기 위해 시스템 전체를 뒤지는 낭비를 줄입니다.
 config.warn_about_missing_glyphs = false
 -- 최신 웹GPU(WebGpu) 그래픽 API를 강제하여 프레임 드랍을 막습니다.
 config.front_end = "WebGpu"
 config.webgpu_power_preference = "HighPerformance"
--- 모니터 주사율(Hz)에 맞춰 최대 프레임을 제한해 GPU 과열을 방지합니다.
+-- 모니터 주사율(Hz)에 맞춰 최대 프레임을 제한해 GPU 과열을 방지
 -- (144Hz 모니터라면 144로 변경 가능, 기본 대개 60~120)
 config.max_fps = 120
--- 커서가 깜빡일 때 부드럽게 감속하는 계산을 꺼서 CPU 소모를 줄입니다.
+-- 커서가 깜빡일 때 부드럽게 감속하는 계산을 꺼서 CPU 최적화
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
 config.cursor_blink_rate = 0
--- 탭 바를 단순화하여 불필요한 UI 드로잉 성능을 아낍니다.
-config.use_fancy_tab_bar = false
 
 return config
